ac5d53aa7688e8280ad8b049983080a53d0c9344
mikekauffman <mike.a.kauffman@gmail.com>
2014-04-04 13:18:32 -0600

    pure refactoring for more specific labeling

--- lib/recipe_converter.rb
+++ lib/recipe_converter.rb
@@ -5,11 +5,11 @@ class RecipeConverter
   end
 
   def to_tablespoons
-  @array.each {|hash| hash.each {|ing, grams| hash[ing] = grams/15}}
+  @array.each {|hash| hash.each {|ingredient, grams| hash[ingredient] = grams/15}}
   end
 
   def to_cups
-  @array.each {|hash| hash.each {|ing, tbs| hash[ing] = tbs/16}}
+  @array.each {|hash| hash.each {|ingredient, tbs| hash[ingredient] = tbs/16}}
   end
 
 end
--- spec/recipe_converter_spec.rb
+++ spec/recipe_converter_spec.rb
@@ -3,8 +3,8 @@ require 'recipe_converter'
 describe RecipeConverter do
 
   it 'converts units of measure from grams to tbs within an array of hashes' do
-    g_hash = RecipeConverter.new([{:cinnamon => 15, :sugar => 30, :flour => 45, :starch => 90}])
-    expect(g_hash.to_tablespoons).to eq [{:cinnamon => 1, :sugar => 2, :flour => 3, :starch => 6}]
+    grams_hash = RecipeConverter.new([{:cinnamon => 15, :sugar => 30, :flour => 45, :starch => 90}])
+    expect(grams_hash.to_tablespoons).to eq [{:cinnamon => 1, :sugar => 2, :flour => 3, :starch => 6}]
   end
 
   it 'converts units of measure from tbs to cups within an array of hashes' do
